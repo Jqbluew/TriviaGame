be99a94b1dc7ec612ba7aea906d59afab570e844
JackieP <jqbluew@mac.com>
2018-02-26 17:16:45 -0500

    finished html

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+
+/* 
+Answers
+q1-Answer is C
+q2-Answer is B
+q3-Answer is D
+q4-Answer is B
+q5-Answer is D
+q6-Answer is C
+q7-Answer is B
+q8-Answer is C
+q9-Answer is B
+q10-Answer is C
+*/
